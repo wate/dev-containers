a7c2082e20a3407d5287373da358b2c71fbf233a
wate <pc.poisoning@gmail.com>
2022-08-10 03:14:36 +0900

    update

--- containers/writing_doc_for_mkdoc/.devcontainer/scripts/post_create.sh
+++ containers/writing_doc_for_mkdoc/.devcontainer/scripts/post_create.sh
@@ -1,4 +1,6 @@
-pip3 install --user -r .devcontainer/requirements.txt
+if [ -f requirements.txt ]; then
+  pip3 install --user -r requirements.txt
+fi
 if [ ! -e mkdocs.yml ]; then
   mkdocs new ./
 fi
