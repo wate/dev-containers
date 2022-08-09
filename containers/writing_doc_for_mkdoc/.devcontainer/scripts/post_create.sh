8c6c35d88e8859419f31e140e62fdb3bbdb144cf
wate <pc.poisoning@gmail.com>
2022-08-10 03:33:59 +0900

    update

--- containers/writing_doc_for_mkdoc/.devcontainer/scripts/post_create.sh
+++ containers/writing_doc_for_mkdoc/.devcontainer/scripts/post_create.sh
@@ -1,4 +1,5 @@
 echo 'eval "$(direnv hook bash)"' >>~/.bashrc
+echo 'alias ls="exa --git --header"' >>~/.bashrc
 echo 'eval "$(direnv hook zsh)"' >>~/.zshrc
 if [ -f requirements.txt ]; then
   pip3 install --user -r requirements.txt
