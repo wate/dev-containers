19fe9f3e225201241e8af770242bc0f80d053ec1
wate <pc.poisoning@gmail.com>
2022-08-12 14:34:18 +0900

    update

--- containers/ansible/.devcontainer/scripts/post_create.sh
+++ containers/ansible/.devcontainer/scripts/post_create.sh
@@ -1,6 +1,10 @@
-echo 'eval "$(direnv hook bash)"' >>~/.bashrc
-echo 'alias ls="exa --git --header"' >>~/.bashrc
-echo 'eval "$(direnv hook zsh)"' >>~/.zshrc
+if type "direnv" >/dev/null 2>&1; then
+  echo 'eval "$(direnv hook bash)"' >>~/.bashrc
+fi
+if type "exa" >/dev/null 2>&1; then
+  echo 'alias ls="exa --git --header"' >>~/.bashrc
+fi
+source ~/.bashrc
 if [ -f requirements.txt ]; then
   pip3 install --user -r requirements.txt
 fi
