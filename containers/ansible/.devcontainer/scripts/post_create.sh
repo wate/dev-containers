4c429dd6ebf889f3996c3aac0193fde6f0f9629e
wate <pc.poisoning@gmail.com>
2022-08-13 18:47:01 +0900

    update

--- containers/ansible/.devcontainer/scripts/post_create.sh
+++ containers/ansible/.devcontainer/scripts/post_create.sh
@@ -4,7 +4,17 @@ fi
 if type "exa" >/dev/null 2>&1; then
   echo 'alias ls="exa --git --header"' >>~/.bashrc
 fi
-source ~/.bashrc
 if [ -f requirements.txt ]; then
   pip3 install --user -r requirements.txt
 fi
+pip install --user argcomplete
+echo 'eval $(register-python-argcomplete ansible)' >>~/.bashrc
+echo 'eval $(register-python-argcomplete ansible-config)' >>~/.bashrc
+echo 'eval $(register-python-argcomplete ansible-console)' >>~/.bashrc
+echo 'eval $(register-python-argcomplete ansible-doc)' >>~/.bashrc
+echo 'eval $(register-python-argcomplete ansible-galaxy)' >>~/.bashrc
+echo 'eval $(register-python-argcomplete ansible-inventory)' >>~/.bashrc
+echo 'eval $(register-python-argcomplete ansible-playbook)' >>~/.bashrc
+echo 'eval $(register-python-argcomplete ansible-pull)' >>~/.bashrc
+echo 'eval $(register-python-argcomplete ansible-vault)' >>~/.bashrc
+source ~/.bashrc
