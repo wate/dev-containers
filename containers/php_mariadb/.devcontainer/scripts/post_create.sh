b2b9e499eba4adaa8771f3afde5c1ca4c3628a2f
wate <pc.poisoning@gmail.com>
2022-09-05 01:23:25 +0900

    update

--- containers/php_mariadb/.devcontainer/scripts/post_create.sh
+++ containers/php_mariadb/.devcontainer/scripts/post_create.sh
@@ -4,9 +4,6 @@ fi
 if type "exa" >/dev/null 2>&1; then
     echo 'alias ls="exa --git --header"' >>~/.bashrc
 fi
-if [ -f /usr/local/share/nvm/nvm.sh ]; then
-    echo "source /usr/local/share/nvm/nvm.sh" >>~/.bashrc
-fi
 if [ ! -f ~/.inputrc ]; then
     echo "set completion-ignore-case on">~/.inputrc
 fi
