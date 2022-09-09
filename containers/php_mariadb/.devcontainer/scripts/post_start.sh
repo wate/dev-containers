b7e81b344dea13533d621d09087347a4eeecc8cf
wate <pc.poisoning@gmail.com>
2022-09-09 12:25:04 +0900

    update

--- containers/php_mariadb/.devcontainer/scripts/post_start.sh
+++ containers/php_mariadb/.devcontainer/scripts/post_start.sh
@@ -2,6 +2,6 @@ if type "direnv" >/dev/null 2>&1 && [ -f .envrc ]; then
     direnv allow
 fi
 if [ -f requirements.txt ]; then
-    pip install --user -r requirements.txt
+    pip3 install --user --disable-pip-version-check -r requirements.txt
 fi
 apache2ctl start
